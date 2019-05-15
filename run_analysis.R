c2a10567209b364238d80197bddb7ddd0e2ecbe4
plbuckman35 <47066191+plbuckman35@users.noreply.github.com>
2019-05-15 14:41:43 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,8 @@
 Create one R script called run_analysis.R that does the following.
 
-Merges the training and the test sets to create one data set.
+#Merges the training and the test sets to create one data set.
+data.all <- rbind(data.train, data.test)
+
 Extracts only the measurements on the mean and standard deviation for each measurement.
 Uses descriptive activity names to name the activities in the data set
 Appropriately labels the data set with descriptive variable names.
