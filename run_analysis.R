4efabb6c84f8cfc409065c45c33270178d277f9e
plbuckman35 <47066191+plbuckman35@users.noreply.github.com>
2019-05-15 15:27:56 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,11 @@ Create one R script called run_analysis.R that does the following.
 #Merges the training and the test sets to create one data set.
 data.all <- rbind(data.train, data.test)
 
-Extracts only the measurements on the mean and standard deviation for each measurement.
-Uses descriptive activity names to name the activities in the data set
-Appropriately labels the data set with descriptive variable names.
-From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+mean_std.select <- grep('mean|std', features)
+data.sub <- data.all[,c(1,2,mean_std.select + 2)]
+
+#Uses descriptive activity names to name the activities in the data set
+
+#Appropriately labels the data set with descriptive variable names.
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
