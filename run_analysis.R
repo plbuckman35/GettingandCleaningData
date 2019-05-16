0e0023d543deb7b6f2f6f9ba15d6becb0159bc8a
plbuckman35 <47066191+plbuckman35@users.noreply.github.com>
2019-05-15 22:13:44 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,16 @@
+#PREP
+
+
+
+
 #Create one R script called run_analysis.R that does the following.
 
 #Merges the training and the test sets to create one data set.
-data.all <- rbind(data.train, data.test)
 
 #Extracts only the measurements on the mean and standard deviation for each measurement.
-mean_std.select <- grep('mean|std', features)
-data.sub <- data.all[,c(1,2,mean_std.select + 2)
 
 #Uses descriptive activity names to name the activities in the data set
-activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
-activity.labels <- as.character(activity.labels[,2])
-data.sub$activity <- activity.labels[data.sub$activity]
 
 #Appropriately labels the data set with descriptive variable names.
-name.new <- names(data.sub)
-name.new <- gsub("[(][)]", "", name.new)
-name.new <- gsub("^t", "TimeDomain_", name.new)
-name.new <- gsub("^f", "FrequencyDomain_", name.new)
-name.new <- gsub("Acc", "Accelerometer", name.new)
-name.new <- gsub("Gyro", "Gyroscope", name.new)
-name.new <- gsub("Mag", "Magnitude", name.new)
-name.new <- gsub("-mean-", "Mean", name.new)
-name.new <- gsub("-std-", "StandardDeviation", name.new)
-names(data.sub) <- name.new
-                     
+
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-data.tidy <- aggregate(data.sub[,3:81], by = list(activity = data.sub$activity, subject = data.sub$subject),FUN = mean)
-write.table(x = data.tidy, file = "data_tidy.txt", row.names = FALSE)
