f41768f8d9f8e98d950df4c864c57153c778bdfb
plbuckman35 <47066191+plbuckman35@users.noreply.github.com>
2019-05-17 10:41:23 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,26 +13,26 @@ if (!file.exists("UCI HAR Dataset")) {
 
 #####Create one R script called run_analysis.R that does the following######
 
-#Merges the training and the test sets to create one data set:
+#1 | Merges the training and the test sets to create one data set:
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 testsubject <- rbind(subject_train, subject_test)
-combineddata <- cbind(testsubject, Y, X)
+CombinedData <- cbind(testsubject, Y, X)
 
-#Extracts only the measurements on the mean and standard deviation for each measurement:
-TidyData <- combindeddata %>% select(subject, code, contains("mean"), contains("std"))
+#2 | Extracts only the measurements on the mean and standard deviation for each measurement:
+TidyData <- CombindedData %>% select(subject, code, contains("mean"), contains("std"))
 
-#Uses descriptive activity names to name the activities in the data set:
+#3 | Uses descriptive activity names to name the activities in the data set:
 activities <- read.table(file.path(targetFolder, 'activity_labels.txt'))
 full_data[, 2] <- activities[full_data[,2], 2]
 
-#Appropriately labels the data set with descriptive variable names:
+#4 | Appropriately labels the data set with descriptive variable names:
 names(subjects) <- "subject"
-data.cleaned <- cbind(subjects, y, X)
+data.cleaned <- cbind(subjects, Y, X)
 write.table(data.cleaned, 
             file.path(current.wd, "merged_and_cleaned_dataset.txt"))
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject:
+#5 | From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject:
 write.table(dataTable, "TidyData.txt", row.name=FALSE)
 
 
